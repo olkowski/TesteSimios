dc202d8723c965f2429845deb366428cba0eae68
rolkowski <rodolfotec@gmail.com>
2020-04-06 05:50:46 -0300

    Corrigindo model

--- src/main/java/br/com/ml/testesimios/model/Simian.java
+++ src/main/java/br/com/ml/testesimios/model/Simian.java
@@ -1,8 +1,6 @@
 package br.com.ml.testesimios.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -12,27 +10,21 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Table(name="TB_SIMIAN")
+@Table(name = "TB_SIMIAN")
 @NoArgsConstructor
 public class Simian {
 
 	@Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
-    private Long id;
-	
-	@NotBlank  
-    private String dna;
+	private String dna;
 
-    
-    @NotBlank
-    private String isSimian;
+	@NotBlank
+	private String isSimian;
 
 	public Simian(@NotBlank String dna, @NotBlank String isSimian) {
 		super();
 		this.dna = dna;
-		
+
 		this.isSimian = isSimian;
 	}
 
-    
 }
